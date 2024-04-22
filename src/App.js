2a37a594f2e59386369924f79f3d4f70949a901d
KabeloKgoadi <ksrkgoadi@gmail.com>
2024-04-22 11:58:56 +0200

    updated the homepage path

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
      < Routes>
-      <Route path = '/' element = {<Homepage/>}/>
+      <Route path = '/crud-4' element = {<Homepage/>}/>
       <Route path = '/create' element = {<AddMembers/>}/>
       <Route path = '/update/:id' element = {<UpdateMember/>}/>
      </Routes>
